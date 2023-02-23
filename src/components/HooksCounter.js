5413cd6ef7e44175b1cb93851cbe451687ee7898
Siydulislam <siydulislam333@gmail.com>
2023-02-23 17:45:34 +0600

    change to static counter

--- src/components/HooksCounter.js
+++ src/components/HooksCounter.js
@@ -5,22 +5,22 @@ export default function HooksCounter() {
     const count = useSelector(state => state.value);
     const dispatch = useDispatch();
 
-    const incrementHandler = (value) => {
-        dispatch(increment(value));
+    const incrementHandler = () => {
+        dispatch(increment());
     }
 
-    const decrementHandler = (value) => {
-        dispatch(decrement(value));
+    const decrementHandler = () => {
+        dispatch(decrement());
     }
 
     return (
         <div class="p-4 h-auto flex flex-col items-center justify-center space-y-5 bg-white rounded shadow">
             <div class="text-2xl font-semibold">{count}</div>
             <div class="flex space-x-3">
-                <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={() => incrementHandler(5)}>
+                <button class="bg-indigo-400 text-white px-3 py-2 rounded shadow" onClick={incrementHandler}>
                     Increment
                 </button>
-                <button class="bg-red-400 text-white px-3 py-2 rounded shadow" onClick={() => decrementHandler(2)}>
+                <button class="bg-red-400 text-white px-3 py-2 rounded shadow" onClick={decrementHandler}>
                     Decrement
                 </button>
             </div>
--- src/redux/counter/actions.js
+++ src/redux/counter/actions.js
@@ -1,15 +1,13 @@
 import { DECREMENT, INCREMENT } from "./actionTypes"
 
-export const increment = (value) => {
+export const increment = () => {
     return {
-        type: INCREMENT,
-        payload: value
+        type: INCREMENT
     }
 }
 
 export const decrement = (value) => {
     return {
-        type: DECREMENT,
-        payload: value
+        type: DECREMENT
     }
 }
