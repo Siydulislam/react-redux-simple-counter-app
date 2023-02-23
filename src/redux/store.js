8faf424eddac2324ed2256fa24b87ad2a7f7bd80
Siydulislam <siydulislam333@gmail.com>
2023-02-23 19:01:18 +0600

    adding multiple middlewares - adding third party middlewares in our app

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,8 @@
 import { applyMiddleware, createStore } from "redux";
 import myLogger from "./middlewares/myLogger";
 import rootReducer from "./rootReducer";
+import logger from "redux-logger";
 
-const store = createStore(rootReducer, applyMiddleware(myLogger));
+const store = createStore(rootReducer, applyMiddleware(myLogger, logger));
 
 export default store;
